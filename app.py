66960f8da492c5c5bc1d7e1f8dc2ae80c77d805d
Tony-1126 <tonyz40@nycstudents.net>
2024-09-18 10:13:41 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+countOfWords = len("I dont know what to put here".split())
+print("Count of Words in The Given Sentence:", countOfWords)
