6d97c787e1956d358045040a2c07435d91d90ad5
Tony-1126 <tonyz40@nycstudents.net>
2024-09-19 10:11:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-x = "this is a thing"
+""" x = "this is a thing"
 y= x.split( )
 z = y[0]
 print(y)
-print(z)
+print(z) """
 
-countOfWords = len("I dont know what to put here".split())
-print("Count of Words in The Given Sentence:", countOfWords)
+""" x = input("type something") """
+
+
+list1 = x = input("Type Something")
+# printing length of list1
+print(len(x - 1))
