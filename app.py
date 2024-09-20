e9178c9ac8dd89d188c9e745e8dabfd082cbf329
Tony-1126 <tonyz40@nycstudents.net>
2024-09-20 10:07:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,33 @@ print(z) """
 
 """ x = input("type something") """
 
+""" use the print function in order to put the amount of words. Use the len function to count the amount of words and input to tell the commenter to write something """
 
-list1 = x = input("Type Something")
-# printing length of list1
-print(len(x - 1))
+""" print(len(input("Write Something:").split())) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" x = int(input("Enter A Number"))
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd") """
+
+""" x = float(input("Enter Your Bill"))
+values = [0, 5 , 10 , 15 ,25 ]
+
+if x """
+
+
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
