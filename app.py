0bbd7d87719f15c11fdc7b4ce6de35054f166622
Tony-1126 <tonyz40@nycstudents.net>
2024-09-25 10:04:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,18 +24,37 @@ if x % 2 == 0:
     print("Even")
 else:
     print("Odd") """
-word_values = {
-    "Bad": 0,
+""" word_values = {
+    "Bad": 0.0,
     "Mediocore": 0.15,
     "Great": 0.20,
     "Excellent": 0.25
 }
+x = int(float(input("Enter Your Bill:")))
+y = float(input("How was your service, Excellent, Great, Mediocore, Bad:"))
+print(Bill*y)
 
-y = input("How was your service, Excellent, Great, Mediocore, Bad:")
-x = input("Enter Your Bill:")
-print(x*y)
+word_values = {
+    "Bad": 0.0,
+    "Mediocore": 0.15,
+    "Great": 0.20,
+    "Excellent": 0.25
+} """   
+Tip = input("How Much Would You Like To Tip? 0, 15, 20, or 25 Percent:")
+Bill = float(input("Enter Your Bill:"))
 
-
+tip_as_percent = Tip / 100
+total_tip_amount = Bill * tip_as_percent
+""" if Tip == 0:
+    print((Bill/100)* 0)
+elif Tip == 15:
+    print((Bill*0.15)* 15)
+elif Tip == 20:
+    print((Bill*0.20)* 20)
+elif Tip == 25:
+    print((Bill*0.25)* 25)
+else:
+    print("Choose a valid percentage") """
 
    
 
